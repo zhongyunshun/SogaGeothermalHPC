1fdced56c10d0e95d7a9ecb10db015cb596dc23d
zhongyunshun <zys@berkeley.edu>
2020-01-10 16:45:48 -0800

    Update Geothermal.cc

--- src/Geothermal.cc
+++ src/Geothermal.cc
@@ -3,6 +3,7 @@
 #include <deal.II/base/function.h>
 #include <deal.II/base/logstream.h>
 
+
 #include <deal.II/lac/vector.h>
 #include <deal.II/lac/full_matrix.h>
 #include <deal.II/lac/dynamic_sparsity_pattern.h>
